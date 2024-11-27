afed26daf351e11b2ddb64462ca01223a6b98eab
hugoheml <51268820+hugoheml@users.noreply.github.com>
2024-11-27 09:47:05 +0000

    feat: add find_restaurant utilities

---
+++ utilities/find_restaurant.js
@@ -0,0 +1,19 @@
+import * as crous from "crowous";
+import { getArgs } from "./args.js";
+
+const identifier = getArgs().city;
+if (!identifier) {
+  throw new Error("Please provide a city name");
+}
+
+try {
+  const restaurants = await crous.restaurants(identifier);
+   
+  for (const restaurant of restaurants) {
+    console.log(restaurant.title);
+    console.log("Address:", restaurant.address);
+    console.log("Id:", restaurant.id);
+  }
+} catch (e) {
+  console.log(`No restaurants found for ${identifier}`);
+}
